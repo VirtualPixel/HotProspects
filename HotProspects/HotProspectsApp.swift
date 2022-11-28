8f99dfd0e437200b95f63f70f64477de2da5334e
Justin Wells <wellsmjustin@gmail.com>
2022-11-28 15:19:08 -0500

    initial commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Justin Wells on 11/28/22.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
