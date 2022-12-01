083623469e255d310c0a6fe1a9e55ea4353b23df
Justin Wells <wellsmjustin@gmail.com>
2022-12-01 18:02:04 -0500

    added model view

---
+++ HotProspects/View/ProspectsView.swift
@@ -0,0 +1,61 @@
+//
+//  ProspectsView.swift
+//  HotProspects
+//
+//  Created by Justin Wells on 12/1/22.
+//
+
+import SwiftUI
+
+struct ProspectsView: View {
+    @EnvironmentObject var prospects: Prospects
+    let filter: FilterType
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(filteredProspects) { prospect in
+                    VStack(alignment: .leading) {
+                        Text(prospect.name)
+                            .font(.headline)
+                        Text(prospect.emailAddress)
+                            .foregroundColor(.secondary)
+                    }
+                }
+            }
+        }
+    }
+    
+    var title: String {
+        switch filter {
+        case .none:
+            return "Everyone"
+        case .contacted:
+            return "Contacted people"
+        case .uncontacted:
+            return "Uncontacted People"
+        }
+    }
+    
+    var filteredProspects: [Prospect] {
+        switch filter {
+        case .none:
+            return prospects.people
+        case .contacted:
+            return prospects.people.filter { $0.isContacted }
+        case .uncontacted:
+            return prospects.people.filter { !$0.isContacted }
+        }
+    }
+}
+
+struct ProspectsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProspectsView(filter: .none)
+            .environmentObject(Prospects())
+    }
+}
+
+enum FilterType {
+    case none, contacted, uncontacted
+}
